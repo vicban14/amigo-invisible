4db3a8f6f4450d4392d16b9ba1a8fca655dd2e8e
Victor Bañuls <vicban@MacBook-Pro.local>
2024-07-15 18:14:36 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welcome to the home page!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home'
+});
+</script>
